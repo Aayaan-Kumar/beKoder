4037fa9af602fe44d08657c8fdc93bcb77a6ffb6
Aayaan <102898866+Aayaan-Kumar@users.noreply.github.com>
2022-08-15 08:44:13 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,6 @@ const about = () => {
   return (
     <div>
       <Head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1858086125833373" crossorigin="anonymous"></script>
         <title>BeKoder - About Us</title>
         <meta name="description" content="BeKoder" />
         <link rel="icon" href="/favicon.ico" />
